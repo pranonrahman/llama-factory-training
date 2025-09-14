5187829ec15a06e1e128aee269c3bf695173561a
Raian Rahman <raian.rahman@therapservices.net>
2025-09-14 12:29:27 +0600

    Add max_length for collator

--- src/dataset/collator.py
+++ src/dataset/collator.py
@@ -1,22 +1,32 @@
-from src.dataset.simplify_me_dataset import getimages, get_conversational_formatted_messages
+from src.dataset.simplify_me_dataset import (
+    getimages,
+    get_conversational_formatted_messages,
+)
 
 
 def collate_fn(batch, processor, config):
     entries = [item for item in batch]
-    images = [getimages(item['images']) for item in batch]
+    images = [getimages(item["images"]) for item in batch]
 
-    prompts = [processor.apply_chat_template(get_conversational_formatted_messages(item["instruction"], "user"),
-                                             add_generation_prompt=True)
-               for item in batch]
+    prompts = [
+        processor.apply_chat_template(
+            get_conversational_formatted_messages(item["instruction"], "user"),
+            add_generation_prompt=True,
+        )
+        for item in batch
+    ]
 
-    ids = [item['id'] for item in batch]
+    ids = [item["id"] for item in batch]
 
     # Process batch
-    inputs = processor(text=prompts, images=images, return_tensors='pt', padding=True)
+    inputs = processor(
+        text=prompts,
+        images=images,
+        return_tensors="pt",
+        padding=True,
+        max_length=1024,
+        truncation=True,
+    )
     inputs = {k: v.to("cuda") for k, v in inputs.items()}
 
-    return {
-        'entries': entries,
-        'inputs': inputs,
-        'ids': ids
-    }
+    return {"entries": entries, "inputs": inputs, "ids": ids}
